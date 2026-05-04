4d5770240f4020e7290f62afb77cd36b6acf5034
Damian Kootstra <damiankootstra@hotmail.com>
2026-05-04 13:03:23 +0200

    Push fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    // Sta grotere JSON imports toe in server components
-    largePageDataBytes: 200 * 1024 * 1024,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
